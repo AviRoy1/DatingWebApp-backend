d6d01fa027c4030083fb1c7376e7e4138b983c70
avijit <avijitroy2944@gmail.com>
2023-07-20 09:59:02 +0530

    add

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(mongoSanitize());
 
 app.use(
   cors({
-    origin: "https://dating-web-app-frontend.vercel.app",
+    origin: process.env.frontend,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
